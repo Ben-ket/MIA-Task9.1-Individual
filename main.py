2cd4b221088013f4af8047baf0d451cedc7b69a0
Mostafa-Di4a <mostafaebs1234@gmail.com>
2026-08-25 13:41:07 +0300

    Added dataset and loaded the images in grayscal

---
+++ main.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+
+
+
+
+def disparity():
+    return
+
+
+
+
+def main():
+    img0l = cv2.cvtColor(cv2.imread('dataset/img0/im0.png'),cv2.COLOR_BGR2GRAY)
+    img0r = cv2.cvtColor(cv2.imread('dataset/img0/im1.png'),cv2.COLOR_BGR2GRAY)
+
+    img1l = cv2.cvtColor(cv2.imread('dataset/img1/im0.png'),cv2.COLOR_BGR2GRAY)
+    img1r = cv2.cvtColor(cv2.imread('dataset/img1/im1.png'),cv2.COLOR_BGR2GRAY)
+
+
+
+
+if __name__ == '__main__':
+    main()
