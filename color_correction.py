e68ea4c52f79bd4134a022907f6ac82a5f03ace4
Mostafa-Di4a <124407363+Ben-ket@users.noreply.github.com>
2026-08-27 04:15:57 +0300

    increasing contrast function & added final corrected image

--- color_correction.py
+++ color_correction.py
@@ -18,6 +18,17 @@ def gray_world_white_balance(img):
     return result.astype(np.uint8)
 
 
+def apply_clahe_lab(img):
+    # enhances image contrast using CLAHE on the Lightness channel
+    lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+    l, a, b = cv2.split(lab)
+
+    clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8, 8))
+    l_enhanced = clahe.apply(l)
+
+    lab_enhanced = cv2.merge((l_enhanced, a, b))
+    return cv2.cvtColor(lab_enhanced, cv2.COLOR_LAB2BGR)
+
 def main():
     img = cv2.imread('dataset/side_quest_2.png')
 
@@ -26,7 +37,10 @@ def main():
     # correct heavy cyan/green tint using white Balance
     balanced = gray_world_white_balance(img)
 
-    cv2.imwrite(output_path, balanced)
+    # restoring contrast using local adaptive equalization
+    corrected = apply_clahe_lab(balanced)
+
+    cv2.imwrite(output_path, corrected)
 
 if __name__ == "__main__":
     main()
