37b52cfc3011b80d810ce56decbd4d2c0371d6d2
Mostafa-Di4a <124407363+Ben-ket@users.noreply.github.com>
2026-08-27 04:07:14 +0300

    Inital Commit

---
+++ color_correction.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy as np
+
+def main():
+    img = cv2.imread('dataset/side_quest_2.png')
+
+    output_path = 'side_quest_2_corrected.png'
+
+
+if __name__ == "__main__":
+    main()
